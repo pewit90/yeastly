2ed5e9a01877501c94ea9e85dd9bde2f4cd38ec9
pewit90 <petrawittw@gmail.com>
2022-08-20 12:06:53 +0200

    Basic bread model with test instance

--- src/model/bread.ts
+++ src/model/bread.ts
@@ -1,3 +1,25 @@
+
+interface Trigger {
+    startTime?: Date|undefined;
+    endTime?: Date|undefined;
+}
+
+interface TimerTrigger extends Trigger {
+    durationSeconds: number;
+}
+
+interface EventTrigger extends Trigger {
+    eventName: string; // e.g. when dough has doubled in size
+}
+
+export interface Step {
+    shortName: string;
+    completed: boolean;
+    trigger?: Trigger;
+}
+
 export interface Bread {
+    id: string;
+    steps: Step[];
     
 }
