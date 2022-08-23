cf32db581b37e9590cc7ba6c6277e840a54d0c91
pewit90 <petrawittw@gmail.com>
2022-08-23 10:18:05 +0200

    Add theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { toBread } from '../model/bread';
 import bread1JSON from '../testing/input/bread1.json';
@@ -10,12 +11,25 @@ const bread1 = toBread(bread1JSON);
 const bread2 = toBread(bread2JSON);
 const breads = [bread1, bread2];
 
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#8d6e63',
+    },
+    secondary: {
+      main: '#ff6f00',
+    },
+  },
+});
+
 function App() {
   return (
-    <Routes >
-      <Route path='/' element={<BreadHistory breads={breads} />} />
-      <Route path="/:uuid" element={<BreadView />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes >
+        <Route path='/' element={<BreadHistory breads={breads} />} />
+        <Route path="/:uuid" element={<BreadView />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
