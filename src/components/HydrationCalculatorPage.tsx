163603cf4a14fe7f7cb0ee511050e912e16b14e5
pewit90 <petrawittw@gmail.com>
2022-12-05 23:39:49 +0100

    HydrationCalculator: Improve layout

--- src/components/HydrationCalculatorPage.tsx
+++ src/components/HydrationCalculatorPage.tsx
@@ -1,65 +1,103 @@
 import { Page } from "./Page";
-import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Unstable_Grid2";
 import { Typography } from "@mui/material";
 import { Hydration } from "../model/hydration";
 import React, { useState } from "react";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
 
-function NumberDisplayField(props: { label: string; value: number }) {
-  return (
-    <div style={{ display: "flex", maxWidth: "40%" }}>
-      <div style={{ textAlign: "left" }}>
-        <Typography color={"text.primary"} align="left">
-          {props.label}
-        </Typography>
-      </div>
-      <div style={{ textAlign: "right", flex: 1 }}>
-        <Typography color={"text.secondary"}>{props.value}</Typography>
-      </div>
-    </div>
-  );
+function Label(props: { text: string }) {
+  return <Typography variant="h6">{props.text}</Typography>;
+}
+
+function NumberDisplayField(props: { value: number; unit: string }) {
+  return <Typography align="right">{props.value + props.unit}</Typography>;
 }
 
 function NumberInputField(props: {
-  label: string;
   value: number;
+  unit: string;
   onChange: Function;
 }) {
   return (
-    <TextField
-      id="outlined-number"
-      label={props.label}
-      type="number"
+    <Input
+      fullWidth
       value={props.value}
-      sx={{ m: 2, maxWidth: "40%" }}
+      type="number"
       onChange={(e) => props.onChange(e.target.value)}
+      endAdornment={
+        <InputAdornment position="end">{props.unit}</InputAdornment>
+      }
     />
   );
 }
 
 export function HydrationCalculator(props: {}) {
-  const [hydration, setHydration] = useState(new Hydration(1000, 0.8));
+  const [hydration, setHydration] = useState(new Hydration(1000, 80, 2));
   return (
     <Page title="Hydration Calculator">
-      <Grid>
-        <Grid>
-          <NumberInputField
-            label="Flour"
-            value={hydration.flour}
-            onChange={(newValue: number) => {
-              new Hydration(newValue, hydration.percentage);
-            }}
-          />{" "}
-          <NumberInputField
-            label="Percentage"
-            value={hydration.percentage * 100}
-            onChange={(newValue: number) => {
-              setHydration(new Hydration(hydration.flour, newValue / 100));
-            }}
-          />{" "}
+      <Grid container spacing={2}>
+        <Grid xs={4}>
+          <Label text="Flour" />
         </Grid>
-        <Grid>
-          <NumberDisplayField label="Water" value={hydration.water} />{" "}
+        <Grid xs={8}>
+          <NumberInputField
+            value={hydration.flour}
+            unit="g"
+            onChange={(newValue: number) => {
+              setHydration(
+                new Hydration(
+                  newValue,
+                  hydration.waterPercentage,
+                  hydration.saltPercentage
+                )
+              );
+            }}
+          />
+        </Grid>
+        <Grid xs={4}>
+          <Label text="Water" />
+        </Grid>
+        <Grid xs={4}>
+          <NumberDisplayField value={hydration.water} unit="ml" />
+        </Grid>
+        <Grid xs={4}>
+          {" "}
+          <NumberInputField
+            value={hydration.waterPercentage}
+            unit="%"
+            onChange={(newValue: number) => {
+              setHydration(
+                new Hydration(
+                  hydration.flour,
+                  newValue,
+                  hydration.saltPercentage
+                )
+              );
+            }}
+          />
+        </Grid>
+        <Grid xs={4}>
+          <Label text="Salt" />
+        </Grid>
+        <Grid xs={4}>
+          <NumberDisplayField value={hydration.salt} unit="g" />
+        </Grid>
+        <Grid xs={4}>
+          {" "}
+          <NumberInputField
+            value={hydration.saltPercentage}
+            unit="%"
+            onChange={(newValue: number) => {
+              setHydration(
+                new Hydration(
+                  hydration.flour,
+                  hydration.waterPercentage,
+                  newValue
+                )
+              );
+            }}
+          />
         </Grid>
       </Grid>
     </Page>
--- src/model/hydration.ts
+++ src/model/hydration.ts
@@ -4,11 +4,16 @@ export class Hydration {
   [immerable] = true;
 
   get water(): number {
-    return this.flour * this.percentage;
+    return this.flour * (this.waterPercentage / 100);
+  }
+
+  get salt(): number {
+    return this.flour * (this.saltPercentage / 100);
   }
 
   constructor(
     public readonly flour: number,
-    public readonly percentage: number
+    public readonly waterPercentage: number,
+    public readonly saltPercentage: number
   ) {}
 }
